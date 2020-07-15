c9303a41d59425098c83a79275c8beacafa95cc6
farookjintha <farookjintha@gmail.com>
2020-07-15 22:58:13 +0530

    initial commit

---
+++ src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signin = () => (
+    <Layout title="Sign In Page" description = "Sign-in to Node React E-commerce App">
+        ....
+    </Layout>
+);
+
+export default Signin; 
