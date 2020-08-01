fa86777ba8e6f6a6d6753ee9566830b657278150
farookjintha <farookjintha@gmail.com>
2020-08-02 01:49:02 +0530

    display filteredresults with images

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
 import Card from './Card';
-import {getCategories} from './apiCore';
+import { getCategories, getFilteredProducts } from './apiCore';
+import { prices } from './fixedPrices';
 import Checkbox from './Checkbox';
+import RadioBox from './RadioBox'
 
 const Shop = () => {
     const [myFilters, setMyFilters] = useState({
@@ -10,6 +12,9 @@ const Shop = () => {
     });
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
+    const [limit, setLimit] = useState(6);
+    const [skip, setSkip] = useState(0);
+    const [filteredResults, setFilteredResults] = useState([]);
 
     const init = () => {
         getCategories().then(data => {
@@ -21,16 +26,46 @@ const Shop = () => {
         }).catch(err => console.log(err));
     }
 
+    const loadFilteredResults = (newFilters) => {
+        getFilteredProducts(skip, limit, newFilters).then(data =>{
+            if(data.error){
+                setError(data.error);
+            }else{
+                setFilteredResults(data.data);
+            }
+        })
+    }
+
     useEffect (() => {
         init();
+        loadFilteredResults(skip, limit, myFilters.filter);
     }, []);
 
     const handleFilters = (filters, filterBy) =>{
         // console.log("SHOP ", filters, filterBy);
         const newFilters = {...myFilters}
         newFilters.filters[filterBy] = filters;
+
+        if(filterBy == "price"){
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+
+        loadFilteredResults(myFilters.filters);
         setMyFilters(newFilters);
 
+    };
+
+    const handlePrice = value => {
+        const data = prices;
+        let array = [];
+
+        for(let key in data){
+            if(data[key]._id === parseInt(value)){
+                array = data[key].array;
+            }
+        }
+        return array;
     }
 
     return(
@@ -41,9 +76,20 @@ const Shop = () => {
                     <ul>
                         <Checkbox categories={categories} handleFilters = {filters => handleFilters(filters, "category")} />
                     </ul>
+                    <h4>Filter by price</h4>
+                    <div>
+                        <RadioBox prices={prices} handleFilters = {filters => handleFilters(filters, "price")} />
+                    </div>
                 </div>
                 <div className="col-8">
-                    {JSON.stringify(myFilters)}
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                        {filteredResults.map((product, i) => (
+                            <div key={i} className="col-6 mb-3">
+                                <Card key={i} product={product} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </Layout>
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -19,3 +19,22 @@ export const getCategories = () => {
     })
     .catch(error => console.log(error));
 }
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+    const data = {
+        limit, skip, filters
+    }
+
+    return fetch(`${API}/products/by/search`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => {
+            return res.json()
+        })
+        .catch(error => console.log(error));
+}
